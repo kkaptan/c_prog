374a52653752fc170457a56c2bf9944e7cc9ee86
Matthew Zammit <me@zammitmatthew.com>
2021-12-28 21:01:45 -0500

    Adds implementation to choose for odd, even or all
    'e' displays even numbers
    'o' displays odd numbers
    Any other letter displays all the numbers between 1 and 100

--- oth/hundred_board.c
+++ oth/hundred_board.c
@@ -7,15 +7,18 @@ int main (void){
   int i;
   char sel;
 
-  sel = 'o';
+  sel = 'x';
 
   for (i = 1; i <= 100; i++){
+
     if ((sel == 'e' && i % 2 == 0) || (sel == 'o' && i % 2 !=0)){
-     printf("%3.2d ", i);   
+      printf("%3.2d ", i);   
     }
-    else if ((sel == 'e' && i % 2 != 0)||(sel == 'o' && i % 2 == 0)){ 
-      printf("    ");
+    else if ((sel == 'e' && i % 2 != 0) || (sel == 'o' && i % 2 == 0)){ 
+      printf(" __ ");
     }
+    else
+      printf("%3.2d ", i);   
 
     if (i % 10 == 0){
       printf("\n");
